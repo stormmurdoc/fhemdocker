763396df31c4383c43436e1658124a2aa6cd90f9
stormmurdoc <murdoc@storm-clan.de>
2020-02-16 18:01:30 +0100

    update

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Description: Setup the environment and start the Docker stack
+#
+# Author: murdoc@storm-clan.de
+#
+# Configuration file: none
+#
+# Parameters: none
+#
+
+SCRIPT=$(basename $0)
+
+echo "+++ $SCRIPT started +++"
+
+# setup access rights
+echo "+++ Setup access rights +++"
+chmod -R 755 ./reverseproxy
+
+echo "+++ Starting docker-compose +++"
+docker-compose up -d
+echo "+++ $SCRIPT ended +++"
